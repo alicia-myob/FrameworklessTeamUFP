8e338134c0ef0192da20c35dd8c87223c31058a8
alicia-myob <Alicia.Shen@myob.com>
2020-12-09 10:24:52 +1300

    [005][test]Test Get all users in database

--- TestFrameworklessApp/UnitTests/TestGetHandler.cs
+++ TestFrameworklessApp/UnitTests/TestGetHandler.cs
@@ -1,10 +1,21 @@
 using System;
+using System.Collections.Generic;
+using FrameworklessApp;
 using Xunit;
 
 namespace TestFrameworklessApp.UnitTests
 {
     public class TestGetHandler
     {
-        
+        [Fact]
+        public void Get_All_Users_In_Database()
+        {
+            var getHandler = new GetHandler();
+            var userList = new List<User>();
+            userList.Add(new User("Bob", 1));
+            var actual = getHandler.getAllUsers();
+            Assert.Equal("Bob", actual);
+        }
     }
+    
 }
