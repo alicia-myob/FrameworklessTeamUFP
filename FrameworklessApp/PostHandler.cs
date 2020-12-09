a790c70fd433e6f6d5951fa395bfa793ae33164a
alex-ruddell-myob <alex.ruddell@myob.com>
2020-12-10 11:58:25 +1300

    [017][feat][test] Handled adding a duplicate user to databse in PostHandler

--- FrameworklessApp/PostHandler.cs
+++ FrameworklessApp/PostHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace FrameworklessApp
 {
@@ -13,11 +14,29 @@ namespace FrameworklessApp
         
         public void AddUser(string userName)
         {
-            var newList = DataController.GetData(_filepath);
-            var newId = Convert.ToInt32(newList[newList.Count - 1].Id) + 1;
+            var list = DataController.GetData(_filepath);
+
+            if (CheckIfUserExists(userName, list))
+            {
+                var newId = Convert.ToInt32(list[list.Count - 1].Id) + 1;
             
-            newList.Add(new  User(userName, newId.ToString()));
-            DataController.SendData(_filepath, newList);
+                list.Add(new  User(userName, newId.ToString()));
+                DataController.SendData(_filepath, list);
+            }
+            else
+            {
+                throw new ArgumentException("User already exists");
+            }
+        }
+
+        private bool CheckIfUserExists(string userName, List<User> list)
+        {
+            foreach (var user in list)
+            {
+                if (user.Name.Equals(userName)) return false;
+            }
+
+            return true;
         }
     }
 }
